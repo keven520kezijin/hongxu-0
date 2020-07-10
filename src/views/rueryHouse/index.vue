6a35a4f42d07cecddd0fe689c64d62b76b70fdb5
keven <kewen@unisound.com>
2020-07-10 15:04:10 +0800

    Initial commit

---
+++ src/views/rueryHouse/index.vue
@@ -0,0 +1,36 @@
+<!--  -->
+<template>
+  <div>查询房屋</div>
+</template>
+
+<script>
+
+export default {
+components: {},
+data () {
+return {
+
+}
+},
+computed: {},
+watch: {},
+created () {
+
+},
+mounted () {
+
+},
+beforeCreate () {},
+beforeMount () {},
+beforeUpdate () {},
+updated () {},
+beforeDestroy () {},
+destroyed () {},
+activated () {},
+methods: {}
+}
+</script>
+<style lang='less' scoped>
+//@import url(); 引入公共css类
+
+</style>
